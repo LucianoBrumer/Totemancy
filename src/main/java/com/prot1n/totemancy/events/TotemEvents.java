f4785adeba6dbdb3d13782aaa465f8cdb1b9917e
LucianoBrumer <lucianobrumer5@gmail.com>
2022-11-07 04:46:02 -0300

    working wither totem

--- src/main/java/com/prot1n/totemancy/events/TotemEvents.java
+++ src/main/java/com/prot1n/totemancy/events/TotemEvents.java
@@ -1,10 +1,13 @@
 package com.prot1n.totemancy.events;
 
+import com.google.common.eventbus.Subscribe;
 import net.minecraft.client.server.IntegratedServer;
+import net.minecraft.util.profiling.jfr.event.WorldLoadFinishedEvent;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 public class TotemEvents {
@@ -14,27 +17,47 @@ public class TotemEvents {
         server.getCommands().performPrefixedCommand(stack, command);
     }
 
+//    @SubscribeEvent
+//    public void worldLoad(WorldLoadFinishedEvent event){
+//
+//        runCommand((IntegratedServer) server, "gamerule commandBlockOutput false");
+//    }
+
     @SubscribeEvent
-    public void rightClickBlock(PlayerInteractEvent.RightClickBlock event){
-        var entity = event.getEntity();
-        var block = event.getUseBlock();
-        var item = event.getItemStack();
+    public void placeBlock(BlockEvent.EntityPlaceEvent event){
+        var placedBlock = event.getPlacedBlock();
 
-        var server = entity.getServer();
+        if(placedBlock.toString().contains("oak_wither_totem")){
+            var entity = event.getEntity();
+            var server = entity.getServer();
 
-        if(server != null){
-            System.out.println(server.getClass());
-            if(item.getItem().toString() == "oak_wither_totem"){
-                entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 100, 0));
+            var pos = event.getPos();
 
-
-                var pos = entity.position();
-                String command = "summon marker " + (int)(pos.x) + " " + (int)(pos.y) + " " + (int)(pos.z) + "  {Tags:[\"totemancy_wither\"]}";
-                runCommand((IntegratedServer) server, command);
-
-            }
+            String command = "summon marker " + (int)(pos.getX()) + " " + (int)(pos.getY()) + " " + (int)(pos.getZ()) + "  {Tags:[\"totemancy_wither\"]}";
+            runCommand((IntegratedServer) server, command);
+            runCommand((IntegratedServer) server, "gamerule sendCommandFeedback false");
         }
+
     }
+//    @SubscribeEvent
+//    public void rightClickBlock(PlayerInteractEvent.RightClickBlock event){
+//        var entity = event.getEntity();
+//        var block = event.getUseBlock();
+//        var item = event.getItemStack();
+//
+//        var server = entity.getServer();
+//
+//        if(server != null){
+//            if(item.getItem().toString() == "oak_wither_totem"){
+//                entity.addEffect(new MobEffectInstance(MobEffects.WITHER, 100, 0));
+//
+//                var pos = entity.position();
+//                String command = "summon marker " + (int)(pos.x) + " " + (int)(pos.y) + " " + (int)(pos.z) + "  {Tags:[\"totemancy_wither\"]}";
+//                runCommand((IntegratedServer) server, command);
+//
+//            }
+//        }
+//    }
 
     @SubscribeEvent
     public void playerTick(TickEvent.PlayerTickEvent event){
@@ -43,8 +66,9 @@ public class TotemEvents {
         if(server != null){
             var pos = player.position();
 
-            String command = "execute as @a[nbt=!{ActiveEffects:[{Id:20}]}] at @s if entity @e[tag=totemancy_wither,distance=..5] run effect give @s wither 5 0";
-            runCommand((IntegratedServer) server, command);
+            runCommand((IntegratedServer) server, "execute as @e[tag=totemancy_wither] at @s if block ~ ~1 ~ oak_log if block ~ ~-1 ~ oak_log if block ~ ~ ~1 oak_stairs[half=top] if block ~ ~ ~-1 oak_stairs[half=top] if entity @e[tag=totemancy_wither,distance=..5] run effect give @e[nbt=!{ActiveEffects:[{Id:20}]},distance=..5] wither 5 0");
+            runCommand((IntegratedServer) server, "execute as @e[tag=totemancy_wither] at @s if block ~ ~1 ~ oak_log if block ~ ~-1 ~ oak_log if block ~1 ~ ~ oak_stairs[half=top] if block ~-1 ~ ~ oak_stairs[half=top] if entity @e[tag=totemancy_wither,distance=..5] run effect give @e[nbt=!{ActiveEffects:[{Id:20}]},distance=..5] wither 5 0");
+            runCommand((IntegratedServer) server, "execute as @e[tag=totemancy_wither] at @s unless block ~ ~ ~ totemancy:oak_wither_totem run kill @s");
         }
     }
 }
